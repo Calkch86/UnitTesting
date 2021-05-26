f9881438fe36eaac7a3b65b01d5dbea0d2c492c3
Chen Hwa Kong <kongchenhwa@gmail.com>
2021-05-26 18:22:25 +0800

    updated greet file and test

--- greeting.js
+++ greeting.js
@@ -1,9 +1,21 @@
-function Greet (name) {
+function greet(name) {
     if (name === null || name === undefined) return 'Invalid Entry!';
+    
+    if (typeof name === typeof []) {
+        if (name.length > 0) {
+          let names = '';
+          name.forEach((elm) => {
+            names += ', ' + elm;
+          });
+          return 'Hello' + names;
+        }
+        return 'Invalid Entry!';
+    }
 
     if (name === name.toUpperCase()){
         return 'HELLO ' + name + '!';
     }
-    
+
     return 'Hello, ' + name;
 }
+module.exports = greet;
--- greeting.test.js
+++ greeting.test.js
@@ -1,4 +1,4 @@
-import greet from './greeting';
+const greet = require('./greeting');
 
 describe ('test greet () ', function () {
     it('should greet name', function (){
@@ -10,6 +10,15 @@ describe ('test greet () ', function () {
     });
 
     it('should handle uppercase greeting', function(){
-        expect (greet('CALVIIN')).toEqual('HELLO CALVIN!');
+        expect (greet('CALVIN')).toEqual('HELLO CALVIN!');
     });
-})
+
+    it('should handle 2 names', function(){
+        expect(greet(['Calvin', 'Isabella'])).toEqual('Hello, Calvin, Isabella');
+    });
+
+    it('should handle multiple names', function(){
+        expect(greet(['Calvin', 'Isabella', 'Irene', 'Winston', 'Vera'])).toEqual('Hello, Calvin, Isabella, Irene, Winston, Vera');
+    });
+
+});
